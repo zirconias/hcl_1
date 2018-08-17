33d2e73e5e544ac93e86c9f678e1279186efd921
islam mohamed drissi <islam.drissi@gmail.com>
2018-08-17 17:31:30 +0200

    Create response.java

---
+++ src/main/java/com/imdrissi/demo/domain/response.java
@@ -0,0 +1,17 @@
+package com.imdrissi.demo.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class response {
+  private String resultCount;
+
+  private List<Album> results;
+}

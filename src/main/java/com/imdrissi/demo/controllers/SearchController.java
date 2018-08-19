ecf32f8b0910e92cb455cf156cab2e8f8ddc82b1
zirconias <islam.drissi@gmail.com>
2018-08-19 11:15:03 +0200

    adding search aggregation

---
+++ src/main/java/com/imdrissi/demo/controllers/SearchController.java
@@ -0,0 +1,55 @@
+package com.imdrissi.demo.controllers;
+
+import com.imdrissi.demo.config.AsyncConf;
+import com.imdrissi.demo.services.SearchService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+
+@Api(
+    basePath = "/api/search",
+    value = "Search",
+    description = "Search in book and album apis",
+    produces = "application/json")
+@RestController
+@RequestMapping(value = "/api/search")
+@Slf4j
+public class SearchController {
+
+  private final SearchService searchService;
+  private final int search_limit;
+
+  public SearchController(
+      SearchService searchService, @Value("${service.search.limit}") int searchLimit) {
+    this.searchService = searchService;
+    this.search_limit = searchLimit;
+  }
+
+  @ApiOperation(value = "search books and albums related to given term")
+  @Async(AsyncConf.TASK_EXECUTOR_CONTROLLER)
+  @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public CompletableFuture<ResponseEntity> search(@RequestParam String term) {
+    return searchService
+        .search(term, search_limit)
+        .<ResponseEntity>thenApply(ResponseEntity::ok)
+        .exceptionally(handleSearchException);
+  }
+
+  private static Function<Throwable, ResponseEntity> handleSearchException =
+      throwable -> {
+        log.error("Unable to search..", throwable);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+      };
+}
--- src/main/java/com/imdrissi/demo/services/SearchService.java
+++ src/main/java/com/imdrissi/demo/services/SearchService.java
@@ -1,7 +1,17 @@
 package com.imdrissi.demo.services;
 
+import com.imdrissi.demo.config.AsyncConf;
+import com.imdrissi.demo.domain.Album;
+import com.imdrissi.demo.domain.Book;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @Slf4j
@@ -13,5 +23,44 @@ public class SearchService {
   public SearchService(AlbumAsyncService albumAsyncService, BookAsyncService bookAsyncService) {
     this.albumAsyncService = albumAsyncService;
     this.bookAsyncService = bookAsyncService;
+  }
+
+  @Async(AsyncConf.TASK_EXECUTOR_SERVICE)
+  public CompletableFuture<List<?>> search(String term, int resultLimit) {
+    log.info("searching for term {}...", term);
+    CompletableFuture<List<Book>> books = bookAsyncService.getBooksByTerm(term, resultLimit);
+    CompletableFuture<List<Album>> albums = albumAsyncService.getAlbumsByTerm(term, resultLimit);
+
+    CompletableFuture<List<?>> allOf = allOf(books, albums);
+
+    List al = new ArrayList();
+    List<?> result =
+        books
+            .thenCombine(
+                albums,
+                (l1, l2) -> {
+                  al.addAll(l2);
+                  al.addAll(l1);
+                  return al;
+                  //                  return Stream.concat(l1.stream(),
+                  // l2.stream()).collect(Collectors.toList());
+                })
+            .join();
+
+    //    List all = new ArrayList();
+    //    allOf.thenAccept(
+    //        l -> {
+    //          l.forEach(System.out::println);
+    //        });
+
+    //    return allOf.join();
+
+    return allOf;
+  }
+
+  static CompletableFuture<List<?>> allOf(CompletableFuture<?>... cfs) {
+    return CompletableFuture.allOf(cfs)
+        .thenApply(
+            ignore -> Stream.of(cfs).map(CompletableFuture::join).collect(Collectors.toList()));
   }
 }
